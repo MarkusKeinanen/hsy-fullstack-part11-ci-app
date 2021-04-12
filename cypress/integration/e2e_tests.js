bf889f1f4fbee56a0e9b30dccf0c01f8a29ff2aa
Keinänen Markus <markusvk@protonmail.fi>
2021-04-13 00:35:02 +0300

    fixed main.yml

--- cypress/integration/e2e_tests.js
+++ cypress/integration/e2e_tests.js
@@ -1,6 +1,6 @@
 describe('Toska messages', function () {
 	it('Messages page can be opened', function () {
-		cy.visit('http://localhost:5000')
+		cy.visit('http://localhost:8000')
 		cy.contains('Messages').click()
 		cy.contains('Send!')
 	})
